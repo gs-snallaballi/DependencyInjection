8199ca01037029c3a06fe37beb3450482575fbbd
snallaballi <snallaballi@gainsight.com>
2018-07-03 16:48:34 +0530

    Dependency Injection Project CheckIn

---
+++ src/test/java/com/gainsight/depinjection/DepInjectionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gainsight.depinjection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DepInjectionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
